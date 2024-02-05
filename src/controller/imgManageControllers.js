236976e10fed5c8f227e9167b29f1b455f2b224c
Bam <khanguyen1204@gmail.com>
2024-02-05 20:39:28 +0700

    add api get-created-img

--- src/controller/imgManageControllers.js
+++ src/controller/imgManageControllers.js
@@ -26,4 +26,26 @@ const getSavedImgById = async (req, res) => {
   }
 };
 
-export { getInfoUser, getSavedImgById };
+const getCreatedImgById = async (req, res) => {
+  try {
+    let { nguoi_dung_id } = req.params;
+    let nguoi_dung_id2 = Number(nguoi_dung_id);
+    let data = await prisma.hinh_anh.findMany({
+      where: {
+        nguoi_dung_id: nguoi_dung_id2,
+      },
+    });
+    res.send(data);
+  } catch (e) {
+    res.send(`getCreatedImgById error ${e}`);
+  }
+};
+
+const deleteImgById = async (req, res) => {
+  try {
+  } catch (e) {
+    req.send(`deleteImgById error ${e}`);
+  }
+};
+
+export { getInfoUser, getSavedImgById, getCreatedImgById };
--- src/routes/imgManageRoutes.js
+++ src/routes/imgManageRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  getCreatedImgById,
   getInfoUser,
   getSavedImgById,
 } from "../controller/imgManageControllers.js";
@@ -8,5 +9,6 @@ const imgManageRoutes = express.Router();
 
 imgManageRoutes.get("/get-info-user", getInfoUser);
 imgManageRoutes.get("/get-saved-img/:nguoi_dung_id", getSavedImgById);
+imgManageRoutes.get("/get-created-img/:nguoi_dung_id", getCreatedImgById);
 
 export default imgManageRoutes;
